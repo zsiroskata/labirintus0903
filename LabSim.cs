9021b2425f7815974df96aae4c4218284d5ee7fc
zsiroskata <78079466+zsiroskata@users.noreply.github.com>
2024-09-03 11:22:25 +0200

    Add project files.

---
+++ LabSim.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace lab0903
+{
+    internal class LabSim
+    {
+        private List<string> adatsorok;
+        private char[,] lab;
+
+        public void AdatsorokBeolvasasa(string forras)
+        {
+            using StreamReader sr = new(forras);
+
+            while (!sr.EndOfStream)
+            {
+                adatsorok.Add(sr.ReadLine());
+            }
+        }
+        public LabSim(string fileName)
+        {
+            AdatsorokBeolvasasa ( new(@$"..\..\..\src\{fileName}")); 
+        }
+        
+    }
+}
